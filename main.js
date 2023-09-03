ad333e7caaf1f61bf945383fc88c8bbb1cdae87a
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-03 20:42:27 +1000

    swap all if else for switch

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+const blackBtn = document.querySelector("#black-button");
+const rainbowBtn = document.querySelector("#rainbow-button");
+const eraseBtn = document.querySelector("#erase-button");
+const clearGridBtn = document.querySelector("#clear-button");
+
+let defaultColor = "black";
+
 document.addEventListener("DOMContentLoaded", function () {
   createGrid(16);
 
-  const gridSizeBtn = document.querySelector("#sizeBtn");
+  const gridSizeBtn = document.querySelector("#size-button");
   gridSizeBtn.addEventListener("click", () => {
     const size = resizeGrid();
     createGrid(size);
   });
 });
-
-// Function to create the grid
 
 function createGrid(size) {
   const gridContainer = document.querySelector(".grid-container");
@@ -17,26 +22,65 @@ function createGrid(size) {
   gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   gridContainer.innerHTML = ""; // Clear existing grid
 
-  let numberOfDivs = size * size;
+  const numberOfDivs = size * size;
   for (let i = 0; i < numberOfDivs; i++) {
     const div = document.createElement("div");
+    div.addEventListener("mouseover", colorTheDiv);
     gridContainer.appendChild(div); // Use instead of below
     // gridContainer.insertAdjacentElement("beforeend", div);
   }
 }
 
-// Resize the grid with user input
-
 function resizeGrid() {
   const promptMessage = document.querySelector("#prompt-message");
-  const userInput = prompt("Please enter a number between 1 and 99.");
-  const number = parseInt(userInput);
+  const userPrompt = prompt("Please enter a number between 1 and 99.");
+  const userInput = parseInt(userPrompt);
 
-  if (isNaN(number) || number < 1 || number > 99) {
-    promptMessage.textContent = "Invalid Entry. Please enter a number between 1 and 99.";
-    return null;
-  } else {
-    promptMessage.textContent = "";
-    return number;
+  // if (userInput < 1 || userInput > 99) {
+  //   promptMessage.textContent = "Invalid Entry. Please enter a number between 1 and 99.";
+  //   return null;
+  // } else if (isNaN(userInput)) {
+  //   promptMessage.textContent = "Hey quit playin, and give me those digits!";
+  // } else {
+  //   promptMessage.textContent = "";
+  //   return userInput;
+  // }
+
+  switch (true) {
+    case isNaN(userInput):
+      promptMessage.textContent = "Hey quit playin, and give me those digits!";
+      break;
+    case userInput < 1 || userInput > 99:
+      promptMessage.textContent = "Please enter a number between 1 and 99.";
+      break;
+    default:
+      promptMessage.textContent = "";
+      return userInput;
   }
 }
+
+function setColor(colorChoice) {
+  defaultColor = colorChoice;
+}
+
+function colorTheDiv() {
+  // if (defaultColor === "rainbow") {
+  //   this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  // } else {
+  //   this.style.backgroundColor = "black";
+  // }
+  switch (true) {
+    case defaultColor === "rainbow":
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      break;
+    default:
+      this.style.backgroundColor = "black";
+  }
+}
+
+blackBtn.addEventListener("click", () => {
+  setColor("black");
+});
+rainbowBtn.addEventListener("click", () => {
+  setColor("rainbow");
+});
