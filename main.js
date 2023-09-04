8904de3b4cc953e19320f93a6fdd2313a9fbd9cd
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-04 14:52:59 +1000

    refactor code

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 const blackBtn = document.querySelector("#black-button");
 const rainbowBtn = document.querySelector("#rainbow-button");
-const eraseBtn = document.querySelector("#erase-button");
+const whiteBtn = document.querySelector("#white-button");
 const clearGridBtn = document.querySelector("#clear-button");
+
+const gridContainer = document.querySelector(".grid-container");
+const gridSizeBtn = document.querySelector("#size-button");
+const promptMessage = document.querySelector("#prompt-message");
 
 let defaultColor = "black";
 
-document.addEventListener("DOMContentLoaded", function () {
-  createGrid(16);
+document.addEventListener("DOMContentLoaded", () => {
+  createGrid(32);
 
-  const gridSizeBtn = document.querySelector("#size-button");
   gridSizeBtn.addEventListener("click", () => {
     const size = resizeGrid();
     createGrid(size);
   });
+
+  blackBtn.addEventListener("click", () => setColor("black"));
+  whiteBtn.addEventListener("click", () => setColor("white"));
+  rainbowBtn.addEventListener("click", () => setColor("rainbow"));
+  clearGridBtn.addEventListener("click", () => clearGrid());
 });
 
 function createGrid(size) {
-  const gridContainer = document.querySelector(".grid-container");
-
+  gridContainer.textContent = ""; // Clear existing grid
   gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  gridContainer.innerHTML = ""; // Clear existing grid
 
   const numberOfDivs = size * size;
   for (let i = 0; i < numberOfDivs; i++) {
     const div = document.createElement("div");
+    div.classList.add("colored-divs");
     div.addEventListener("mouseover", colorTheDiv);
     gridContainer.appendChild(div); // Use instead of below
     // gridContainer.insertAdjacentElement("beforeend", div);
@@ -32,30 +39,15 @@ function createGrid(size) {
 }
 
 function resizeGrid() {
-  const promptMessage = document.querySelector("#prompt-message");
   const userPrompt = prompt("Please enter a number between 1 and 99.");
   const userInput = parseInt(userPrompt);
 
-  // if (userInput < 1 || userInput > 99) {
-  //   promptMessage.textContent = "Invalid Entry. Please enter a number between 1 and 99.";
-  //   return null;
-  // } else if (isNaN(userInput)) {
-  //   promptMessage.textContent = "Hey quit playin, and give me those digits!";
-  // } else {
-  //   promptMessage.textContent = "";
-  //   return userInput;
-  // }
-
-  switch (true) {
-    case isNaN(userInput):
-      promptMessage.textContent = "Hey quit playin, and give me those digits!";
-      break;
-    case userInput < 1 || userInput > 99:
-      promptMessage.textContent = "Please enter a number between 1 and 99.";
-      break;
-    default:
-      promptMessage.textContent = "";
-      return userInput;
+  if (isNaN(userInput) || userInput < 1 || userInput > 99) {
+    promptMessage.textContent = "Invalid Entry. Please enter a number between 1 and 99.";
+    return null;
+  } else {
+    promptMessage.textContent = "";
+    return userInput;
   }
 }
 
@@ -64,23 +56,19 @@ function setColor(colorChoice) {
 }
 
 function colorTheDiv() {
-  // if (defaultColor === "rainbow") {
-  //   this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  // } else {
-  //   this.style.backgroundColor = "black";
-  // }
-  switch (true) {
-    case defaultColor === "rainbow":
+  switch (defaultColor) {
+    case "rainbow":
       this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      break;
+    case "white":
+      this.style.backgroundColor = "white";
       break;
     default:
       this.style.backgroundColor = "black";
   }
 }
 
-blackBtn.addEventListener("click", () => {
-  setColor("black");
-});
-rainbowBtn.addEventListener("click", () => {
-  setColor("rainbow");
-});
+function clearGrid() {
+  const coloredDivs = document.querySelectorAll(".colored-divs");
+  coloredDivs.forEach((div) => (div.style.backgroundColor = ""));
+}
